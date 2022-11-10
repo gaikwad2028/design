2f40bbc1824fa51e5631aefcf02493f28fd5584f
gaikwad2028 <108988732+gaikwad2028@users.noreply.github.com>
2022-11-10 20:50:31 +0530

    third

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <>
     <div className="App">
-      <First/>
       <Routes>
       <Route exact path='/first' element={<First />} />
      <Route exact path='/secon' element={<Secon />} />
